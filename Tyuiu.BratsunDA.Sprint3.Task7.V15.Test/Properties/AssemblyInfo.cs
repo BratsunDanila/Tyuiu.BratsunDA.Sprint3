54e1c71dfc24f91a2f352da92ba566a552a1f8f0
BratsunDanila <Tyuiu.BratsunDA@outlook.com>
2023-10-29 23:17:48 +0500

    Task7

---
+++ Tyuiu.BratsunDA.Sprint3.Task7.V15.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.BratsunDA.Sprint3.Task7.V15.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.BratsunDA.Sprint3.Task7.V15.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ccaf5d23-c8ad-4888-9252-2c0c3fe8aaa6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
